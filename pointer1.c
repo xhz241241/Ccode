0c16ba51b04b96d8e63a3fa040d4da0b308ae593
xhz241241 <1063251507@qq.com>
2020-11-06 13:27:50 +0800

    c语言指针代码

---
+++ pointer1.c
@@ -0,0 +1,21 @@
+////求Sn=a+aa+aaa+aaaa+aaaaa的前5项之和，其中a是一个数字，
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main() {
+//	int num = 0;
+//	int tem = 0;
+//	int sum = 0;
+//	printf("请输入一个0~9的整数:\n");
+//	scanf("%d", &num);
+//	for (int i = 0; i < 5; i++) {
+//		tem = tem * 10 + num;
+//		printf("%d + ", tem);
+//		sum = sum + tem;
+//	}
+//	printf("= %d\n", sum);
+//
+//	system("pause");
+//	return 0;
+//}
